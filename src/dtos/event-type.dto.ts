cd6ed93def677730782d55848a06acf995d7caa4
benil09 <benil8235@gmail.com>
2026-06-30 13:50:27 +0530

    adde repo layer for event types

---
+++ src/dtos/event-type.dto.ts
@@ -0,0 +1,19 @@
+
+import {z} from 'zod'
+
+export const createEventTypeSchema = z.object({
+    title:z.string().min(1,"Title cannot be empty").max(100,"Title cannot exceed 100 characters"),
+    description:z.string().max(1000).optional(),
+    locationType:z.enum(['online','in-person']).default('online'),
+    locationValue:z.string().optional(),
+    durationMin:z.number().min(15).max(120).default(30),
+    isActive:z.boolean().default(true),
+    bufferBeforeMin:z.number().optional(),
+    bufferAfterMin:z.number().optional(),
+    slug:z.string().min(1).regex(/^[a-z0-9-]+$/, "Slug must only contain lowercase letters, numbers, and hyphens")
+})
+
+export type CreateEventTypeDto = z.infer<typeof createEventTypeSchema>
+
+export const updateEventTypeSchema = createEventTypeSchema.partial();
+export type UpdateEventTypeDto = z.infer<typeof updateEventTypeSchema>;
---
+++ src/repositories/event-type.repository.ts
@@ -0,0 +1,67 @@
+import { prisma } from "../config/database.js";
+import {CreateEventTypeDto,UpdateEventTypeDto} from "../dtos/event-type.dto.js"
+
+export async function getEventTypesByUserIdRepo(hostId: number) {
+    const response = await prisma.eventTypes.findMany({
+        where: {
+            hostId
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+    return response;
+}
+
+export async function findEventTypeByEventIdRepo(id: number) {
+    const eventType = await prisma.eventTypes.findUnique({
+        where: {
+            id
+        }
+       
+    })
+     return eventType;
+}
+
+export async function createEventTypeRepo(hostId:number, data: CreateEventTypeDto) {
+    const newEvent = await prisma.eventTypes.create({
+      data : {
+        ...data,hostId
+      }
+    })
+    return newEvent;
+ }
+
+export async function updateEventTypeRepo(eventId:number,data: UpdateEventTypeDto) {
+    const updateEvent = await prisma.eventTypes.update({
+        where:{id:eventId},
+        data:data
+    })
+    return updateEvent
+ }
+
+export async function deleteEventTypeRepo(eventId: number) { 
+    const deleteEvent = await prisma.eventTypes.delete({
+        where:{id:eventId}
+    })
+    return deleteEvent;
+}
+
+export async function findByHostAndSlug(hostId: number, slug: string) {
+    const response = await prisma.eventTypes.findFirst({
+        where : {
+            hostId,
+            slug
+        }
+    })
+    return response;
+}
+
+export async function findActiveByHostIdAndEventSlug(hostId: number, slug: string) { }
+
+export async function slugExistsForHost(hostId: number, slug: string) { 
+    
+}
+
+export async function findActiveEventTypesByHost(hostId: number) { }
